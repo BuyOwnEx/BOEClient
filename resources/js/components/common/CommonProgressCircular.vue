3851a85ee64d111161b8036c2ef0818a0f5a1700
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-02-22 07:30:06 +0500

    update

---
+++ resources/js/components/common/CommonProgressCircular.vue
@@ -0,0 +1,20 @@
+<template>
+	<div class='text-center mt-3'>
+		<v-progress-circular :color='color' :size='size' :width='width' indeterminate />
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'CommonProgressCircular',
+
+	props: {
+		color: {
+			type: String,
+			default: 'primary',
+		},
+		size: { type: String, default: '30' },
+		width: { type: String, default: '2' },
+	},
+};
+</script>
