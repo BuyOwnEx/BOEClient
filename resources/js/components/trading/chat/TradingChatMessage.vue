7b3f6af79e6b5de1883908a3b69e409cab31a31f
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-04 06:46:45 +0500

    Подгонка шрифта сообщений к общему стилю страницы

--- resources/js/components/trading/chat/TradingChatMessage.vue
+++ resources/js/components/trading/chat/TradingChatMessage.vue
@@ -1,12 +1,12 @@
 <template>
-	<v-list-item class='tcm px-0'>
-		<v-list-item-content class='pt-0'>
-			<v-list-item-title class='tcm__title text--disabled'>
-				<span class='tcm__title__author'> {{ message.author }}: </span>
-				<span class='tcm__title__date'>{{ formatMessageDate() }}</span>
+	<v-list-item class="tcm px-0">
+		<v-list-item-content class="pt-0">
+			<v-list-item-title class="tcm__title text--disabled">
+				<span class="tcm__title__author"> {{ message.author }}: </span>
+				<span class="tcm__title__date">{{ formatMessageDate() }}</span>
 			</v-list-item-title>
 
-			<div class='tcm__text'>
+			<div class="tcm__text">
 				{{ message.text }}
 			</div>
 		</v-list-item-content>
@@ -37,20 +37,20 @@ export default {
 };
 </script>
 
-<style scoped lang='scss'>
+<style scoped lang="scss">
 .tcm {
 	&__title {
 		display: flex;
 		justify-content: space-between;
-		font-size: 0.9rem;
+		font-size: 0.85rem;
 
 		&__date {
-			font-size: 0.8rem;
+			font-size: 0.65rem;
 		}
 	}
 
 	&__text {
-		font-size: 0.95rem;
+		font-size: 0.85rem;
 	}
 }
 </style>
