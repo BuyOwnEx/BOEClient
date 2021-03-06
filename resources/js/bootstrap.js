2b6ced370ae018f7c6c3cb2e66510ecf96bef4c5
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-06 18:38:56 +0500

    Добавление импорта axios-interceptors

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -9,6 +9,7 @@ window._ = require('lodash');
 window.axios = require('axios');
 
 window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+require('./axios-interceptors.js');
 window.moment = require('moment-timezone');
 
 /**
