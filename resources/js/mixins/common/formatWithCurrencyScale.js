99bf9a4166d1faf78ebb5c096bafabcc3ae5d587
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-10 06:09:06 +0500

    Удаление комментов

--- resources/js/mixins/common/formatWithCurrencyScale.js
+++ resources/js/mixins/common/formatWithCurrencyScale.js
@@ -4,9 +4,8 @@ BigNumber.config({ EXPONENTIAL_AT: [-15, 20] });
 export default {
 	methods: {
 		formatWithCurrencyScale(value, currency) {
-			console.log('init', value, currency);
 			const scale = this.findCurrencyScale(currency.toUpperCase());
-			console.log('scale', scale);
+
 			if (scale) {
 				return BigNumber(value).toFixed(scale, 1);
 			} else {
@@ -16,20 +15,14 @@ export default {
 
 		findCurrencyScale(currency) {
 			const allCurrencies = this.$store.state.trading.all_currencies;
-			console.log(this.$store.state.trading);
-			if (allCurrencies) {
-				console.log('curr, all curr', currency, allCurrencies);
+			const isCurrenciesInit = this.$store.state.trading.allCurrencyListInit;
 
-				const storeCurrencyItem = allCurrencies.find(currencyItem => {
-					console.log(
-						'find',
-						currencyItem.currency.toUpperCase(),
-						currency.toUpperCase()
-					);
-					return currencyItem.currency.toUpperCase() === currency.toUpperCase();
-				});
+			if (isCurrenciesInit) {
+				const storeCurrencyItem = allCurrencies.find(
+					currencyItem =>
+						currencyItem.currency.toUpperCase() === currency.toUpperCase()
+				);
 
-				console.log('store item', storeCurrencyItem);
 				if (storeCurrencyItem) return storeCurrencyItem.scale;
 			}
 		},
