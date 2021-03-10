34d50d10adaa02af225f562370a3b4f045727074
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-10 06:09:39 +0500

    Замена currency на market для функции высчитывания scale

--- resources/js/components/trading/forms/ask/AskLimitForm.vue
+++ resources/js/components/trading/forms/ask/AskLimitForm.vue
@@ -234,7 +234,7 @@
 				<div class="alf__footer__available text-center">
 					{{ $t('trading.order.available') }}
 					<span class="available_balance" @click="setAmount(100)">
-						{{ formatWithCurrencyScale(balance, currency) }}
+						{{ formatWithCurrencyScale(balance, market) }}
 					</span>
 					{{ currency.toUpperCase() }}
 				</div>
