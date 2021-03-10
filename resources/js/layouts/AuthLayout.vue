4b31d08d3fe0dd76580c5aef921788e61dfa208b
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-10 17:35:03 +0500

    Добавление выбора языка и смены темы в авторизации

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -2,6 +2,11 @@
 	<div
 		class="auth-layout d-flex text-center flex-column flex-md-row flex-grow-1"
 	>
+		<div class="auth-layout__header">
+			<ToolbarLanguage />
+			<ToolbarThemeChanger />
+		</div>
+
 		<v-sheet
 			class="layout-side mx-auto d-none d-md-flex flex-md-column justify-space-between"
 			style="background: linear-gradient(5deg, #142f6d 0%, rgba(255, 255, 255, 0) 60%);"
@@ -39,18 +44,25 @@
 <script>
 import { mapState } from 'vuex';
 
+import ToolbarThemeChanger from '../components/toolbar/ToolbarThemeChanger';
+import ToolbarLanguage from '../components/toolbar/ToolbarLanguage';
+
 export default {
+	components: { ToolbarThemeChanger, ToolbarLanguage },
+
 	data() {
 		return {};
 	},
+
 	computed: {
 		...mapState('app', ['product']),
 	},
+
 	methods: {},
 };
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .layout-side {
 	width: 420px;
 }
@@ -69,6 +81,13 @@ export default {
 }
 
 .auth-layout {
-	background-color: #222;
+	background-color: #ababab21;
+	&__header {
+		position: absolute;
+		top: 32px;
+		right: 64px;
+		bottom: 0;
+		z-index: 1;
+	}
 }
 </style>
