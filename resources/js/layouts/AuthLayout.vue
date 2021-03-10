1dc205fd4157b5d3c2e271104393f59681917af6
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-10 15:28:53 +0500

    Изменение цвета фона на более серый

--- resources/js/layouts/AuthLayout.vue
+++ resources/js/layouts/AuthLayout.vue
@@ -1,25 +1,37 @@
 <template>
-	<div class='d-flex text-center flex-column flex-md-row flex-grow-1'>
-		<v-sheet class='layout-side mx-auto d-none d-md-flex flex-md-column justify-space-between'
-											style='background: linear-gradient(5deg, #142f6d 0%, #091c48 40%);'>
-			<canvas class='waves' id='waves'></canvas>
-			<div class='mt-3 mt-md-10 pa-2'>
-				<div class='display-2 font-weight-bold primary--text'>
+	<div
+		class="auth-layout d-flex text-center flex-column flex-md-row flex-grow-1"
+	>
+		<v-sheet
+			class="layout-side mx-auto d-none d-md-flex flex-md-column justify-space-between"
+			style="background: linear-gradient(5deg, #142f6d 0%, #091c48 40%);"
+		>
+			<canvas class="waves" id="waves"></canvas>
+
+			<div class="mt-3 mt-md-10 pa-2">
+				<div class="display-2 font-weight-bold primary--text">
 					{{ product.name }}
 				</div>
-				<div class='title my-2 white--text'>be the future. be the crypto</div>
-				<v-btn href='/' class='my-4'>Take me back</v-btn>
+
+				<div class="title my-2 white--text">be the future. be the crypto</div>
+
+				<v-btn href="/" class="my-4">Take me back</v-btn>
 			</div>
-			<div class='pa-2 mb-1'>
-				<v-img max-width='250' src='/images/logo_full.png' class='ma-auto'></v-img>
+
+			<div class="pa-2 mb-1">
+				<v-img max-width="250" src="/images/logo_full.png" class="ma-auto" />
 			</div>
 		</v-sheet>
 
-		<div class='pa-2 pa-md-4 flex-grow-1 align-center justify-center d-flex flex-column'>
-			<div class='layout-content ma-auto w-full'>
+		<div
+			class="pa-2 pa-md-4 flex-grow-1 align-center justify-center d-flex flex-column"
+		>
+			<div class="layout-content ma-auto w-full">
 				<slot></slot>
 			</div>
-			<div class='overline mt-4'>{{ product.name }} - {{ product.version }}</div>
+			<div class="overline mt-4">
+				{{ product.name }} - {{ product.version }}
+			</div>
 		</div>
 	</div>
 </template>
@@ -54,5 +66,9 @@ export default {
 	top: 0;
 	left: -2px;
 	height: 100vh;
+}
+
+.auth-layout {
+	background-color: #222;
 }
 </style>
