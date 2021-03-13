f90488e3a29706d349a7c144ecdbd1dd6df07f78
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-13 07:14:19 +0500

    Реализация автоматического открытия панели, вложенных панелей

--- resources/js/components/docs/DocsContent.vue
+++ resources/js/components/docs/DocsContent.vue
@@ -3,12 +3,42 @@
 		<!--		-->
 
 		<slot>
-			<div>
+			<div v-if="nested">
+				<div
+					v-for="(dataItem, index) in dataArray"
+					:id="dataItem.id"
+					:key="dataItem.id"
+				>
+					<div class="title pb-2">
+						{{ dataItem.title }}
+					</div>
+					<v-expansion-panels
+						v-model="nestedOpenedPanels[index]"
+						class="mb-4"
+						multiple
+					>
+						<v-expansion-panel
+							v-for="(item, i) in dataItem.items"
+							:key="i"
+							:id="item.id"
+						>
+							<v-expansion-panel-header class="font-weight-black">
+								{{ item.title }}
+							</v-expansion-panel-header>
+							<v-expansion-panel-content>
+								{{ item.content }}
+							</v-expansion-panel-content>
+						</v-expansion-panel>
+					</v-expansion-panels>
+				</div>
+			</div>
+
+			<div v-else>
 				<div v-for="dataItem in dataArray" :id="dataItem.id" :key="dataItem.id">
 					<div class="title pb-2">
 						{{ dataItem.title }}
 					</div>
-					<v-expansion-panels class="mb-4" multiple>
+					<v-expansion-panels v-model="openedPanels" class="mb-4" multiple>
 						<v-expansion-panel
 							v-for="(item, i) in dataItem.items"
 							:key="i"
@@ -41,6 +71,17 @@ export default {
 				return [];
 			},
 		},
+		nested: {
+			type: Boolean,
+			default: false,
+		},
+	},
+
+	data() {
+		return {
+			openedPanels: [0],
+			nestedOpenedPanels: [[0]],
+		};
 	},
 };
 </script>
