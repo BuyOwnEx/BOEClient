750cf2fea25919265fb6537b7209605ca3b53b09
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-15 19:49:05 +0500

    docs: изменение стилей заголовка содержания и рефакторинг

--- resources/js/components/docs/DocsNavigation.vue
+++ resources/js/components/docs/DocsNavigation.vue
@@ -1,48 +1,33 @@
 <template>
 	<v-col class="docs-navigation" cols="12" md="3" xl="2">
 		<div class="docs-navigation__sticky-wrapper">
-			<div class="title mb-2">
+			<div class="trading-component-title mb-2">
 				{{ $t('docs.navigation') }}
 			</div>
 			<!---->
 
 			<slot>
-				<div v-if="nested">
-					<v-treeview
-						v-model="tree"
-						:items="dataArray"
-						:open="['general']"
-						:transition="true"
-						item-children="items"
-						item-text="title"
-						open-on-click
-						dense
-					>
-						<template #label="{ item }">
-							<v-btn
-								color="primary"
-								text
-								plain
-								@click.stop="$vuetify.goTo(`#${item.id}`, { offset: 30 })"
-							>
-								{{ item.title }}
-							</v-btn>
-						</template>
-					</v-treeview>
-				</div>
-
-				<div v-else>
-					<div v-for="item in dataArray[0].items" :key="item.id" class="mb-1">
+				<v-treeview
+					v-model="tree"
+					:items="dataArray"
+					:open="['general']"
+					:transition="true"
+					item-children="items"
+					item-text="title"
+					open-on-click
+					dense
+				>
+					<template #label="{ item }">
 						<v-btn
 							color="primary"
 							text
 							plain
-							@click="$vuetify.goTo(`#${item.id}`, { offset: 30 })"
+							@click.stop="$vuetify.goTo(`#${item.id}`, { offset: 30 })"
 						>
 							{{ item.title }}
 						</v-btn>
-					</div>
-				</div>
+					</template>
+				</v-treeview>
 			</slot>
 
 			<!---->
@@ -60,10 +45,6 @@ export default {
 			default() {
 				return [];
 			},
-		},
-		nested: {
-			type: Boolean,
-			default: false,
 		},
 	},
 
