b673b90ac89deb5577fd2ad2b7ba1d7e91197860
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-18 08:37:48 +0500

    profile: изменение отступов как на других страницах

--- resources/js/pages/Profile.vue
+++ resources/js/pages/Profile.vue
@@ -1,48 +1,48 @@
 <template>
-	<v-container class="profile" fluid>
-		<div class='flex-grow-1'>
-			<v-tabs v-model='tab' :show-arrows='true' background-color='transparent'>
-				<v-tab href='#account'>Account</v-tab>
-				<v-tab href='#information'>Information</v-tab>
-			</v-tabs>
+	<div class="profile-page flex-grow-1">
+		<v-tabs v-model="selectedTab" background-color="transparent" show-arrows>
+			<v-tab :key="1">Account</v-tab>
+			<v-tab :key="2">Information</v-tab>
+		</v-tabs>
 
-			<v-tabs-items v-model='tab'>
-				<v-tab-item value='account'>
-					<account-tab ref='account' :user='user'></account-tab>
-				</v-tab-item>
+		<v-tabs-items v-model="selectedTab">
+			<v-tab-item :key="1">
+				<AccountTab :user="user" />
+			</v-tab-item>
 
-				<v-tab-item value='information'>
-					<information-tab ref='information' :user='user'></information-tab>
-				</v-tab-item>
-			</v-tabs-items>
-		</div>
-	</v-container>
+			<v-tab-item :key="2">
+				<InformationTab :user="user" />
+			</v-tab-item>
+		</v-tabs-items>
+	</div>
 </template>
 
 <script>
-	import AccountTab from '../pages/users/EditUser/AccountTab';
-	import InformationTab from '../pages/users/EditUser/InformationTab';
+import AccountTab from '../pages/users/EditUser/AccountTab';
+import InformationTab from '../pages/users/EditUser/InformationTab';
 
 export default {
 	name: 'Profile',
+
 	components: {
 		AccountTab,
 		InformationTab,
 	},
+
 	data() {
 		return {
 			user: {
-				'id': 32,
-				'email': 'bfitchew0@ezinearticles.com',
-				'name': 'Bartel Fitchew',
-				'verified': false,
-				'created': '2019-08-09T03:14:12Z',
-				'lastSignIn': '2019-08-14T20:00:53Z',
-				'disabled': true,
-				'role': 'ADMIN',
-				'avatar': '/images/avatars/avatar1.svg',
+				id: 32,
+				email: 'bfitchew0@ezinearticles.com',
+				name: 'Bartel Fitchew',
+				verified: false,
+				created: '2019-08-09T03:14:12Z',
+				lastSignIn: '2019-08-14T20:00:53Z',
+				disabled: true,
+				role: 'ADMIN',
+				avatar: '/images/avatars/avatar1.svg',
 			},
-			tab: null,
+			selectedTab: 0,
 			breadcrumbs: [
 				{
 					text: 'Users',
