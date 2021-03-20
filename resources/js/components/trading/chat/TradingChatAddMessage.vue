7205ce5024a9659bc2769d852c510e1d97363eab
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-20 19:57:24 +0500

    trading: фикс counter и цветов полосочек

--- resources/js/components/trading/chat/TradingChatAddMessage.vue
+++ resources/js/components/trading/chat/TradingChatAddMessage.vue
@@ -6,7 +6,6 @@
 			placeholder="Your message..."
 			maxlength="512"
 			counter="512"
-			hide-details
 		/>
 
 		<v-btn
@@ -63,7 +62,7 @@ export default {
 	display: flex;
 	justify-content: space-between;
 	align-items: center;
-	border-top: 1px solid #9090908f;
+	border-top: 1px solid #90909057;
 	margin-top: auto;
 	padding-top: 4px;
 
@@ -75,6 +74,16 @@ export default {
 	&__send {
 		display: flex;
 		align-items: center;
+	}
+
+	::v-deep.v-input__slot {
+		margin-bottom: 2px !important;
+		&::before {
+			border-color: #90909057 !important;
+		}
+	}
+	::v-deep.v-counter {
+		font-size: 9px !important;
 	}
 }
 </style>
