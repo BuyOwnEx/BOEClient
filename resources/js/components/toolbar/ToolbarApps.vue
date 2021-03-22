f7a8249a00be0c7716c0109ef4702158f095b990
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-22 07:38:29 +0500

    global: change app list in toolbar

--- resources/js/components/toolbar/ToolbarApps.vue
+++ resources/js/components/toolbar/ToolbarApps.vue
@@ -1,64 +1,54 @@
 <template>
-	<v-menu offset-y left transition='slide-y-transition'>
-		<template v-slot:activator='{ on }'>
-			<v-btn icon v-on='on'>
+	<v-menu offset-y left transition="slide-y-transition">
+		<template v-slot:activator="{ on }">
+			<v-btn icon v-on="on">
 				<v-icon>mdi-view-grid-outline</v-icon>
 			</v-btn>
 		</template>
 
-		<v-card class='d-flex flex-row flex-wrap' style='max-width: 280px'>
-			<div
-				v-for='app in apps'
-				:key='app.link'
-				class='app-tile pa-3 text-center'
-				style='flex: 0 50%'
-				@click='navigateTo(app.link)'
-			>
-				<v-icon color='primary'>{{ app.icon }}</v-icon>
-				<div class='font-weight-bold mt-1'>{{ app.key ? $t(app.key) : app.text }}</div>
-				<div class='caption'>{{ app.subtitleKey ? $t(app.subtitleKey) : app.subtitle }}</div>
-			</div>
+		<v-card>
+			<v-list dense>
+				<v-list-item-group>
+					<v-list-item v-for="item in items" :key="item.title" :href="item.to">
+						<v-list-item-avatar>
+							<v-icon color="primary" large>{{ item.icon }}</v-icon>
+						</v-list-item-avatar>
+						<v-list-item-content>
+							<v-list-item-title>{{ item.title }}</v-list-item-title>
+							<v-list-item-subtitle class="toolbar-apps__item-subtitle">
+								{{ item.subtitle }}
+							</v-list-item-subtitle>
+						</v-list-item-content>
+					</v-list-item>
+				</v-list-item-group>
+			</v-list>
 		</v-card>
 	</v-menu>
 </template>
 
 <script>
 import config from '../../configs';
-/*
-|---------------------------------------------------------------------
-| Toolbar Apps Component
-|---------------------------------------------------------------------
-|
-| Quickmenu for applications in the toolbar
-|
-*/
+
 export default {
 	data() {
 		return {
 			apps: config.toolbar.apps,
+			items: [
+				{
+					title: 'Exchange',
+					subtitle: 'Торговля криптоактивами',
+					icon: 'mdi-apps',
+					to: '/',
+				},
+			],
 		};
-	},
-	methods: {
-		navigateTo(path) {
-			window.location.href = path;
-		},
 	},
 };
 </script>
 
-<style lang='scss' scoped>
-.app-tile {
-	display: flex;
-	justify-content: center;
-	align-items: center;
-	flex-direction: column;
-	cursor: pointer;
-	border-radius: 6px;
-	background-color: var(--v-background-base);
-	transition: transform 0.2s;
-
-	&:hover {
-		transform: scale(1.1);
-	}
-}
+<style lang="sass" scoped>
+.toolbar-apps
+	&__item-subtitle
+		font-size: 0.8rem !important
+		color: rgba(157, 157, 157, 0.9) !important
 </style>
