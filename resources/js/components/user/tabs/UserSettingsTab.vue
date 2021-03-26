2ccbee9366af2c84d6d7b722490c2c4c7aaa3caa
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-26 09:46:42 +0500

    profile: add settings tab mock content

--- resources/js/components/user/tabs/UserSettingsTab.vue
+++ resources/js/components/user/tabs/UserSettingsTab.vue
@@ -1,15 +1,72 @@
 <template>
-	<v-card>
-		<v-card-title> User Settings </v-card-title>
+	<v-card class="user-settings-tab">
+		<v-card-title> {{ $t('user.title.settings') }} </v-card-title>
 
-		<v-card-text> </v-card-text>
+		<v-card-text>
+			<v-form @submit.prevent="saveSettings">
+				<section class="user-settings-tab__notifications">
+					<div class="user-settings-tab__header">
+						Управление почтовыми оповещениями
+					</div>
+
+					<v-checkbox hide-details label="checkbox" />
+					<v-radio-group hide-details mandatory dense row>
+						<v-radio :ripple="false" label="radio" />
+						<v-radio :ripple="false" label="radio" />
+					</v-radio-group>
+				</section>
+
+				<section class="user-settings-tab__system">
+					<div class="user-settings-tab__header">Настрйоки системы</div>
+
+					<v-checkbox hide-details label="checkbox" />
+					<v-radio-group hide-details mandatory dense row>
+						<v-radio :ripple="false" label="radio" />
+						<v-radio :ripple="false" label="radio" />
+					</v-radio-group>
+				</section>
+
+				<section class="user-settings-tab__security">
+					<div class="user-settings-tab__header">Настрйоки безопасности</div>
+
+					<v-checkbox hide-details label="checkbox" />
+					<v-radio-group hide-details mandatory dense row>
+						<v-radio :ripple="false" label="radio" />
+						<v-radio :ripple="false" label="radio" />
+					</v-radio-group>
+				</section>
+
+				<v-btn type="submit">{{ $t('common.save') }}</v-btn>
+			</v-form>
+		</v-card-text>
 	</v-card>
 </template>
 
 <script>
 export default {
 	name: 'UserSettingsTab',
+
+	data() {
+		return {};
+	},
+
+	methods: {
+		async saveSettings() {
+			console.log('saveSettings');
+		},
+	},
 };
 </script>
 
-<style scoped></style>
+<style lang="sass" scoped>
+.user-settings-tab
+	&__header
+		font-size: 0.9rem
+		font-weight: 500
+
+	section
+		padding-bottom: 32px
+
+	::v-deep.v-input
+		margin-top: 8px
+</style>
