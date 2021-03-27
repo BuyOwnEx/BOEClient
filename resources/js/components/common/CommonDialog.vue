45d69e8e37fcda9cbb8035189a44f66e28595d5a
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-27 12:50:57 +0500

    common: add common dialog

---
+++ resources/js/components/common/CommonDialog.vue
@@ -0,0 +1,87 @@
+<template>
+	<v-dialog v-model="dialog" width="500">
+		<template v-slot:activator="{ on, attrs }">
+			<span v-bind="attrs" v-on="on">
+				<slot></slot>
+			</span>
+		</template>
+
+		<v-card>
+			<slot name="card">
+				<v-card-title class="common-dialog__title">
+					<slot name="title"></slot>
+				</v-card-title>
+
+				<v-card-text class="common-dialog__content">
+					<slot name="content"></slot>
+				</v-card-text>
+
+				<v-divider />
+
+				<v-card-actions class="common-dialog__actions">
+					<slot name="actions">
+						<v-spacer />
+
+						<slot name="close">
+							<v-btn text @click="close">
+								Закрыть
+							</v-btn>
+						</slot>
+
+						<v-spacer />
+
+						<slot name="confirm">
+							<v-btn
+								:color="confirmColor"
+								text
+								@click="confirm"
+							>
+								{{ confirmText }}
+							</v-btn>
+						</slot>
+
+						<v-spacer />
+					</slot>
+				</v-card-actions>
+			</slot>
+		</v-card>
+	</v-dialog>
+</template>
+
+<script>
+export default {
+	name: 'CommonDialog',
+
+	props: {
+		confirmText: {
+			type: String,
+			required: false,
+			default: 'Принять',
+		},
+		confirmColor: {
+			type: String,
+			required: false,
+			default: 'primary',
+		},
+	},
+
+	data() {
+		return {
+			dialog: false,
+		};
+	},
+
+	methods: {
+		confirm() {
+			this.$emit('confirm');
+			this.close();
+		},
+
+		close() {
+			this.dialog = false;
+		},
+	},
+};
+</script>
+
+<style scoped></style>
