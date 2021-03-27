e42aa01743e6a5fb66c0946d91d05eec33f19322
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-27 12:53:35 +0500

    profile: make api list and item (edit action need logic fix)

--- resources/js/components/user/tabs/api/parts/UserApiItem.vue
+++ resources/js/components/user/tabs/api/parts/UserApiItem.vue
@@ -1,76 +1,81 @@
 <template>
 	<v-card class="user-api-item">
-		<v-card-title class="justify-space-between">
-			<div>{{ apiItem.name }}</div>
+		<v-card-title class="user-api-item__title">
+			<div class="user-api-item__name">{{ apiItem.name }}</div>
 
-			<div>
-				<v-btn @click="openEditDialog">Редактировать разрешения</v-btn>
-				<v-btn @click="openDeleteDialog">Удалить</v-btn>
+			<div :class="[$vuetify.rtl ? 'mr-1' : 'ml-1']">
+					<UserApiDialogEdit :api-item="apiItem" @edit="editAPI" />
+					<UserApiDialogDeleteOne
+						:id="apiItem.id"
+						:name="apiItem.name"
+						@delete="deleteAPI"
+					/>
 			</div>
 		</v-card-title>
 
 		<v-divider />
 
-		<v-card-text>
-			<v-row>
-				<v-col cols="0" sm="3" xl="2">
-					<div class="user-api-item__lock-wrapper">
-						<span>
-							<v-avatar
-									class=""
-									size="150"
-									v-html="generatedIcon(apiItem.name)"
-							/>
-						</span>
-					</div>
-				</v-col>
+		<div class="user-api-item__content pa-2">
+			<div class="user-api-item__icon-wrapper">
+				<v-avatar
+					class="user-api-item__icon"
+					size="150"
+					v-html="generatedIcon(apiItem.name)"
+				/>
+			</div>
 
-				<v-col cols="12" sm="9" xl="10">
-					<div class="user-api-item__api-key-wrapper">
-						<div class="user-api-item__header">API Key Hash (SHA 256)</div>
-						<div class="user-api-item__api-key">{{ apiItem.token }}</div>
-					</div>
+			<div class="user-api-item__info-wrapper">
+				<div class="user-api-item__api-key-wrapper">
+					<div class="user-api-item__header">API Key Hash (SHA 256)</div>
+					<div class="user-api-item__api-key">{{ apiItem.token }}</div>
+				</div>
 
-					<div class="user-api-item__secret-key-wrapper">
-						<div class="user-api-item__header">Secret Key (Hidden)</div>
-						<div class="user-api-item__secret-key"> *************** </div>
-					</div>
+				<div class="user-api-item__secret-key-wrapper">
+					<div class="user-api-item__header">Secret Key (Hidden)</div>
+					<div class="user-api-item__secret-key">***************</div>
+				</div>
 
-					<div class="user-api-item__header">Разрешения API</div>
+				<div class="user-api-item__header">Разрешения API</div>
 
-					<div class="user-api-item__abilities-key-wrapper">
-						<div>
-							<v-checkbox
-									class="mt-sm-0"
-									hide-details
-									dense
-									disabled
-									label="Торговля"
-							/>
-							<v-checkbox
-									class="mt-sm-0"
-									hide-details
-									dense
-									disabled
-									label="Вывод средств"
-							/>
-						</div>
+				<div class="user-api-item__abilities-key-wrapper">
+					<v-checkbox
+						class="mt-0"
+						label="Торговля"
+						:value="tradingItemValue"
+						hide-details
+						dense
+						disabled
+					/>
+					<v-checkbox
+						class="mt-0"
+						label="Вывод средств"
+						:value="withdrawItemValue"
+						hide-details
+						dense
+						disabled
+					/>
+				</div>
+
+				<div class="user-api-item__created-key-wrapper">
+					<div class="user-api-item__header">Создан</div>
+					<div class="user-api-item__created">
+						{{ formatDate(apiItem.created_at) }}
 					</div>
-					<div class="user-api-item__created-key-wrapper">
-						<div class="user-api-item__header">Создан</div>
-						<div class="user-api-item__created"> {{ apiItem.created_at }} </div>
+				</div>
+				<div class="user-api-item__updated-key-wrapper">
+					<div class="user-api-item__header">Обновлен</div>
+					<div class="user-api-item__updated">
+						{{ formatDate(apiItem.updated_at) }}
 					</div>
-					<div class="user-api-item__updated-key-wrapper">
-						<div class="user-api-item__header">Обновлен</div>
-						<div class="user-api-item__updated"> {{ apiItem.updated_at }} </div>
+				</div>
+				<div class="user-api-item__used-key-wrapper">
+					<div class="user-api-item__header">Последнее использование</div>
+					<div class="user-api-item__used">
+						{{ formatDate(apiItem.last_used_at) }}
 					</div>
-					<div class="user-api-item__used-key-wrapper">
-						<div class="user-api-item__header">Последнее использование</div>
-						<div class="user-api-item__used"> {{ apiItem.last_used_at }} </div>
-					</div>
-				</v-col>
-			</v-row>
-		</v-card-text>
+				</div>
+			</div>
+		</div>
 	</v-card>
 </template>
 
@@ -78,8 +83,17 @@
 import Avatars from '@dicebear/avatars';
 import sprites from '@dicebear/avatars-jdenticon-sprites';
 
+import UserApiDialogEdit from './dialog/UserApiDialogEdit';
+import UserApiDialogDeleteOne from './dialog/UserApiDialogDeleteOne';
+
+import formatDate from '../../../../../mixins/format/formatDate';
+
 export default {
 	name: 'UserApiItem',
+
+	components: { UserApiDialogEdit, UserApiDialogDeleteOne },
+
+	mixins: [formatDate],
 
 	props: {
 		apiItem: {
@@ -92,13 +106,27 @@ export default {
 		return {};
 	},
 
+	computed: {
+		tradingItemValue() {
+			return this.apiItem.abilities.indexOf('trading') !== -1;
+		},
+		withdrawItemValue() {
+			return this.apiItem.abilities.indexOf('withdraw') !== -1;
+		},
+	},
+
 	methods: {
-		openEditDialog() {},
-		openDeleteDialog() {},
 		generatedIcon(name) {
 			const options = {};
 			const avatars = new Avatars(sprites, options);
 			return avatars.create(name);
+		},
+
+		editAPI(newAbilities) {
+			this.apiItem.abilities = newAbilities;
+		},
+		deleteAPI() {
+			this.$emit('delete', this.apiItem.id);
 		},
 	},
 };
@@ -106,26 +134,61 @@ export default {
 
 <style lang="sass" scoped>
 .user-api-item
+	&__title
+		flex-wrap: nowrap
+		white-space: nowrap
+		justify-content: space-between
 	&__header
 		font-size: 1rem
 		font-weight: 500
 		padding-top: 16px
 		padding-bottom: 4px
 
-	&__lock-wrapper
-		border: 1px solid rgba(103,103,103,0.32)
-		height: 150px
-		width: 150px
-		padding: 8px
+	&__name
+		text-overflow: ellipsis
+		overflow: hidden
 
+	&__content
+		display: grid
+		grid-template-columns: 180px 1fr
+		grid-template-areas: 'icon info'
+
+	&__icon-wrapper
+		display: flex
+		grid-area: icon
+	&__info-wrapper
+		grid-area: info
+		overflow: hidden
+
+	&__icon
+		padding: 8px
+		border: 1px solid rgba(103,103,103,0.32)
+		border-radius: 0
+
+	&__abilities-key-wrapper
+		margin-left: -4px
+
+	&__api-key-wrapper
+		.user-api-item__header
+			padding-top: 0
 	&__api-key
 		font-size: 1.1rem
+
 	&__secret-key
 		font-size: 1.1rem
 		font-weight: bold
 
-	&__limits-form
-		.v-input--checkbox
-			display: inline-flex
-			width: 33%
+	@media screen and (max-width: 600px)
+		&__content
+			display: flex
+			flex-flow: column
+		&__icon-wrapper
+			justify-content: center
+			margin-bottom: 32px
+
+.v-application--is-rtl
+	.user-api-item
+		&__abilities-key-wrapper
+			margin-left: 0
+			margin-right: -4px
 </style>
