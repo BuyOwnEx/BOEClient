86fafee5c5f26f65fca7175ad4554f3c61944b10
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-27 14:30:07 +0500

    profile: move sumsub to verification tab

--- resources/js/components/user/tabs/UserVerificationTab.vue
+++ resources/js/components/user/tabs/UserVerificationTab.vue
@@ -62,10 +62,14 @@
 				</v-form>
 			</v-card-text>
 		</v-card>
+
+		<v-card class="mt-4" id="sumsub-websdk-container"></v-card>
 	</div>
 </template>
 
 <script>
+import snsWebSdk from '@sumsub/websdk';
+
 export default {
 	name: 'UserVerificationTab',
 
@@ -73,6 +77,13 @@ export default {
 		date: '1990-10-09',
 		menu: false,
 		gender: 'male',
+
+		SumSubToken: '',
+		api: 'https://test-api.sumsub.com',
+		flowName: 'scheme',
+		email: '',
+		phone: null,
+		i18n: null,
 	}),
 
 	watch: {
@@ -85,6 +96,89 @@ export default {
 		save(date) {
 			this.$refs.menu.save(date);
 		},
+
+		newSumSubToken: async function() {
+			let token = await axios.get('/trader/ext/sumsub_token');
+			this.SumSubToken = token.data.token;
+			return token.data.token;
+		},
+		launchWebSdk(
+			apiUrl,
+			flowName,
+			accessToken,
+			applicantEmail,
+			applicantPhone,
+			customI18nMessages
+		) {
+			let snsWebSdkInstance = snsWebSdk
+				.Builder(apiUrl, flowName)
+				.withAccessToken(accessToken, newSumSubTokenCallback => {
+					// Access token expired
+					// get a new one and pass it to the callback to re-initiate the WebSDK
+					let newSumSubToken = this.newSumSubToken(); // get a new token from your backend
+					newSumSubTokenCallback(newSumSubToken);
+				})
+				.withConf({
+					lang: 'en',
+					email: applicantEmail,
+					phone: applicantPhone,
+					i18n: customI18nMessages,
+					onMessage: (type, payload) => {
+						// see below what kind of messages the WebSDK generates
+						console.log('WebSDK onMessage', type, payload);
+					},
+					uiConf: {
+						//customCss: "https://buyownex.com/css/sumsub.css"
+						customCssStr: `
+							:root {
+								--success-color: #06d6a0;
+								--red-color: #ef476f;
+							}
+
+							input, select, textarea {
+								color: #404254;
+								background: none
+							}
+
+							.input-field span, .radio-group span {
+								font-size: 11px;
+							}
+							.phone-input .phone {
+								box-shadow: none !important;
+							}
+							.content {
+								background: none;
+							}
+							.input-field span, .radio-group span {
+								color: #404254;
+							}
+							`,
+						// URL to css file in case you need change it dynamically from the code
+						// the similar setting at Applicant flow will rewrite customCss
+						// you may also use to pass string with plain styles `customCssStr:`
+					},
+					onError: error => {
+						console.error('WebSDK onError', error);
+					},
+				})
+				.build();
+			// you are ready to go:
+			// just launch the WebSDK by providing the container element for it
+			snsWebSdkInstance.launch('#sumsub-websdk-container');
+		},
+	},
+
+	async mounted() {
+		let self = this;
+		let token = await this.newSumSubToken();
+		this.launchWebSdk(
+			self.api,
+			self.flowName,
+			token,
+			self.email,
+			self.phone,
+			self.i18n
+		);
 	},
 };
 </script>
--- resources/js/components/user/tabs/api/UserApiTab.vue
+++ resources/js/components/user/tabs/api/UserApiTab.vue
@@ -1,14 +1,10 @@
 <template>
 	<div class="user-api-tab">
 		<UserApiList />
-
-		<div id="sumsub-websdk-container"></div>
 	</div>
 </template>
 
 <script>
-import snsWebSdk from '@sumsub/websdk';
-
 import UserApiList from './parts/UserApiList';
 
 export default {
@@ -16,100 +12,6 @@ export default {
 
 	components: {
 		UserApiList,
-	},
-
-	data() {
-		return {
-			SumSubToken: '',
-			api: 'https://test-api.sumsub.com',
-			flowName: 'scheme',
-			email: '',
-			phone: null,
-			i18n: null,
-		};
-	},
-	methods: {
-		newSumSubToken: async function() {
-			let token = await axios.get('/trader/ext/sumsub_token');
-			this.SumSubToken = token.data.token;
-			return token.data.token;
-		},
-		launchWebSdk(
-			apiUrl,
-			flowName,
-			accessToken,
-			applicantEmail,
-			applicantPhone,
-			customI18nMessages
-		) {
-			let snsWebSdkInstance = snsWebSdk
-				.Builder(apiUrl, flowName)
-				.withAccessToken(accessToken, newSumSubTokenCallback => {
-					// Access token expired
-					// get a new one and pass it to the callback to re-initiate the WebSDK
-					let newSumSubToken = this.newSumSubToken(); // get a new token from your backend
-					newSumSubTokenCallback(newSumSubToken);
-				})
-				.withConf({
-					lang: 'en',
-					email: applicantEmail,
-					phone: applicantPhone,
-					i18n: customI18nMessages,
-					onMessage: (type, payload) => {
-						// see below what kind of messages the WebSDK generates
-						console.log('WebSDK onMessage', type, payload);
-					},
-					uiConf: {
-						//customCss: "https://buyownex.com/css/sumsub.css"
-						customCssStr: `
-							:root {
-								--success-color: #06d6a0;
-								--red-color: #ef476f;
-							}
-
-							input, select, textarea {
-								color: #404254;
-								background: none
-							}
-
-							.input-field span, .radio-group span {
-								font-size: 11px;
-							}
-							.phone-input .phone {
-								box-shadow: none !important;
-							}
-							.content {
-								background: none;
-							}
-							.input-field span, .radio-group span {
-								color: #404254;
-							}
-							`,
-						// URL to css file in case you need change it dynamically from the code
-						// the similar setting at Applicant flow will rewrite customCss
-						// you may also use to pass string with plain styles `customCssStr:`
-					},
-					onError: error => {
-						console.error('WebSDK onError', error);
-					},
-				})
-				.build();
-			// you are ready to go:
-			// just launch the WebSDK by providing the container element for it
-			snsWebSdkInstance.launch('#sumsub-websdk-container');
-		},
-	},
-	async mounted() {
-		let self = this;
-		let token = await this.newSumSubToken();
-		this.launchWebSdk(
-			self.api,
-			self.flowName,
-			token,
-			self.email,
-			self.phone,
-			self.i18n
-		);
 	},
 };
 </script>
