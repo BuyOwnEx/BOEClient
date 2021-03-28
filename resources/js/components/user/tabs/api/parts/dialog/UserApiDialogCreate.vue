4d05144585b4d67b38fff2d88e4890e8b28b5a6f
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-28 11:34:53 +0500

    close #19, close #20: fix create api dialog styling, add abilities checkboxes

--- resources/js/components/user/tabs/api/parts/dialog/UserApiDialogCreate.vue
+++ resources/js/components/user/tabs/api/parts/dialog/UserApiDialogCreate.vue
@@ -1,22 +1,54 @@
 <template>
-	<v-dialog v-model="dialog" width="500">
+	<v-dialog v-model="dialog" width="400">
 		<template v-slot:activator="{ on, attrs }">
-			<v-btn small tile :class="[$vuetify.rtl ? 'ml-1' : 'mr-1']" :loading="creating" color="success" v-bind="attrs" v-on="on">
+			<v-btn
+				small
+				tile
+				:class="[$vuetify.rtl ? 'ml-1' : 'mr-1']"
+				:loading="creating"
+				color="success"
+				v-bind="attrs"
+				v-on="on"
+			>
 				Создать API
 			</v-btn>
 		</template>
 
-		<v-card>
-			<v-card-title class="pb-0">Создать API</v-card-title>
+		<v-card class="user-api-dialog-create">
+			<v-card-title class="user-api-dialog-create__header">
+				Создать API
+			</v-card-title>
 
-			<v-form ref="form" class="user-api-create__form" @submit.prevent="create">
+			<v-form
+				v-model="valid"
+				ref="form"
+				class="user-api-dialog-create__form"
+				@submit.prevent="create"
+			>
 				<v-card-text>
 					<v-text-field
 						v-model="form.name"
+						class="mt-0 pt-1"
 						label="API name"
-						hide-details
-						outlined
+						counter="64"
+						:rules="[rules.required, rules.max64char]"
 						dense
+						autofocus
+					/>
+
+					<v-checkbox
+						v-model="trading"
+						:ripple="false"
+						hide-details
+						dense
+						label="Торговля"
+					/>
+					<v-checkbox
+						v-model="withdraw"
+						:ripple="false"
+						hide-details
+						dense
+						label="Вывод средств"
 					/>
 				</v-card-text>
 
@@ -31,7 +63,14 @@
 
 					<v-spacer />
 
-					<v-btn :loading="creating" type="submit" color="primary" small tile text>
+					<v-btn
+						:loading="creating"
+						type="submit"
+						color="primary"
+						small
+						tile
+						text
+					>
 						Создать
 					</v-btn>
 
@@ -43,23 +82,42 @@
 </template>
 
 <script>
+import formValidationRules from '../../../../../../mixins/common/formValidationRules';
+
 export default {
 	name: 'UserApiDialogCreate',
+
+	mixins: [formValidationRules],
 
 	data() {
 		return {
 			dialog: false,
 			creating: false,
+			valid: false,
+
+			trading: false,
+			withdraw: false,
+
 			form: {
 				name: '',
-				abilities: "'trading','withdraw'",
 			},
 		};
 	},
 
+	computed: {
+		enabledAbilities() {
+			const result = [];
+
+			if (this.trading) result.push('trading');
+			if (this.withdraw) result.push('withdraw');
+
+			return result;
+		},
+	},
+
 	methods: {
 		async create() {
-			if (this.form.name.trim() === '') {
+			if (this.form.name.trim() === '' || !this.valid) {
 				this.close();
 				return;
 			}
@@ -67,14 +125,18 @@ export default {
 			try {
 				this.startLoading();
 
+				const formData = {
+					...this.form,
+					abilities: this.enabledAbilities,
+				};
+
 				const { data } = await axios.post(
 					'/trader/ext/new_api_token',
-					this.form
+					formData
 				);
 
 				this.$emit('create', data.data.accessToken);
 				this.close();
-				this.clearForm();
 			} catch (e) {
 				console.error(e);
 			} finally {
@@ -82,9 +144,6 @@ export default {
 			}
 		},
 
-		clearForm() {
-			this.$refs.form.reset();
-		},
 		startLoading() {
 			this.creating = true;
 		},
@@ -93,9 +152,24 @@ export default {
 		},
 		close() {
 			this.dialog = false;
+			this.$refs.form.reset();
 		},
 	},
 };
 </script>
 
-<style scoped></style>
+<style lang="sass" scoped>
+.user-api-dialog-create
+	&__header
+		background: var(--v-success-base)
+		font-weight: 600 !important
+		padding: 8px 24px 8px !important
+
+	::v-deep.v-input--checkbox
+		margin-top: 0
+
+.theme--dark
+	.user-api-dialog-create
+		&__header
+			background: var(--v-success-darken1)
+</style>
