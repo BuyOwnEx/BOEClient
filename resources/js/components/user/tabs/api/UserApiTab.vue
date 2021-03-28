e5903138e69c48326f8532e0081478078a79ca38
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-28 09:48:30 +0500

    close #22: make api tab fill height, add empty text

--- resources/js/components/user/tabs/api/UserApiTab.vue
+++ resources/js/components/user/tabs/api/UserApiTab.vue
@@ -1,29 +1,105 @@
 <template>
-	<div class="user-api-tab">
-		<UserApiList />
-	</div>
+	<v-card class="user-api-tab tab-fill-height">
+		<v-card-title class="user-api-tab__title-wrapper">
+			<div class="user-api-tab__title">API ключи</div>
+
+			<div class="user-api-tab__actions">
+				<UserApiDialogCreate @create="addCreatedAPI" />
+				<UserApiDialogDeleteAll @delete="deleteAllAPI" />
+			</div>
+		</v-card-title>
+
+		<CommonLoading v-if="!api" />
+		<div v-else-if="api && !api.length" class="user-api-tab__empty">
+			<span>API list is empty</span>
+		</div>
+
+		<v-card-text v-else>
+			<UserApiItem
+				class="user-api-tab__item"
+				v-for="item in api"
+				:key="item.id"
+				:api-item="item"
+				@delete="deleteAPIByID"
+			/>
+		</v-card-text>
+	</v-card>
 </template>
 
 <script>
-import UserApiList from './parts/UserApiList';
+import UserApiItem from './parts/UserApiItem';
+import UserApiDialogCreate from './parts/dialog/UserApiDialogCreate';
+import UserApiDialogDeleteAll from './parts/dialog/UserApiDialogDeleteAll';
+import CommonLoading from '../../../common/CommonLoading';
 
 export default {
 	name: 'UserApiTab',
 
 	components: {
-		UserApiList,
+		UserApiItem,
+		UserApiDialogCreate,
+		UserApiDialogDeleteAll,
+		CommonLoading,
 	},
 
-	mounted() {
+	data() {
+		return {
+			api: null,
+		};
+	},
+
+	methods: {
+		addCreatedAPI(newAPI) {
+			this.api.push(newAPI);
+		},
+		deleteAllAPI() {
+			this.api = [];
+		},
+		deleteAPIByID(id) {
+			this.api = this.api.filter(item => item.id !== id);
+		},
+	},
+
+	async mounted() {
 		window.location.hash = '#api';
-	}
+		const { data } = await axios.get('/trader/ext/api_tokens');
+		this.api = data.data;
+	},
 };
 </script>
 
 <style lang="sass" scoped>
 .user-api-tab
+	display: flex
+	flex-flow: column
+
+	&__title-wrapper
+		justify-content: space-between
+
+	&__empty
+		display: flex
+		justify-content: center
+		align-items: center
+		opacity: 0.8
+		margin: auto
+		span
+			margin-bottom: 64px
+
+	&__item
+		margin-bottom: 5px
+		&:last-child
+			margin-bottom: 0
+	::v-deep.v-card
+		box-shadow: none
+		border: 1px solid #7b7b7b52
 	> ::v-deep.v-card
 		margin-bottom: 5px
 		&:last-child
 			margin-bottom: 0
+
+	@media screen and (max-width: 450px)
+		&__title-wrapper
+			display: block
+		&__actions
+			margin-top: 8px
 </style>
