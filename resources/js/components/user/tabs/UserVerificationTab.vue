c88e1370b1a5a0f64e5356a15cab5a7c6f3a96a6
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-03-28 13:29:04 +0500

    close #23: replace old form with sumsub, some restyling

--- resources/js/components/user/tabs/UserVerificationTab.vue
+++ resources/js/components/user/tabs/UserVerificationTab.vue
@@ -1,70 +1,9 @@
 <template>
-	<div class="user-verification-tab tab-fill-height">
-		<v-card>
-			<v-card-title>User Verification</v-card-title>
+	<v-card class="user-verification-tab tab-fill-height">
+		<v-card-title>{{ $t('user.title.verification') }}</v-card-title>
 
-			<v-card-text>
-				<v-form>
-					<v-row>
-						<v-col cols="12" md="6">
-							<v-text-field
-								value="First and two on el street"
-								label="Address Line 1"
-							/>
-							<v-text-field value="" label="Address Line 2" />
-							<v-text-field value="1231" label="Zip Code" />
-							<v-text-field value="Los Angeles" label="City" />
-							<v-text-field value="California" label="State" />
-							<v-text-field value="United States" label="Country" />
-						</v-col>
-
-						<v-col cols="12" md="6">
-							<v-text-field value="+8484548112" label="Phone"></v-text-field>
-							<v-menu
-								ref="menu"
-								v-model="menu"
-								:close-on-content-click="false"
-								transition="scale-transition"
-								offset-y
-								min-width="290px"
-							>
-								<template v-slot:activator="{ on, attrs }">
-									<v-text-field
-										v-model="date"
-										label="Birthday date"
-										readonly
-										v-bind="attrs"
-										v-on="on"
-									/>
-								</template>
-								<v-date-picker
-									ref="picker"
-									v-model="date"
-									:max="new Date().toISOString().substr(0, 10)"
-									min="1950-01-01"
-									@change="save"
-								/>
-							</v-menu>
-							<v-text-field value="https://" label="Website"></v-text-field>
-							<v-radio-group v-model="gender" label="Gender">
-								<v-radio label="Male" value="male" />
-								<v-radio label="Female" value="female" />
-								<v-radio label="Other" value="other" />
-							</v-radio-group>
-						</v-col>
-					</v-row>
-
-					<div class="d-flex">
-						<v-btn>Reset</v-btn>
-						<v-spacer />
-						<v-btn color="primary">Save</v-btn>
-					</div>
-				</v-form>
-			</v-card-text>
-		</v-card>
-
-		<v-card class="mt-4" id="sumsub-websdk-container"></v-card>
-	</div>
+		<div id="sumsub" />
+	</v-card>
 </template>
 
 <script>
@@ -74,10 +13,6 @@ export default {
 	name: 'UserVerificationTab',
 
 	data: () => ({
-		date: '1990-10-09',
-		menu: false,
-		gender: 'male',
-
 		SumSubToken: '',
 		api: 'https://test-api.sumsub.com',
 		flowName: 'scheme',
@@ -86,17 +21,7 @@ export default {
 		i18n: null,
 	}),
 
-	watch: {
-		menu(val) {
-			val && setTimeout(() => (this.$refs.picker.activePicker = 'YEAR'));
-		},
-	},
-
 	methods: {
-		save(date) {
-			this.$refs.menu.save(date);
-		},
-
 		newSumSubToken: async function() {
 			let token = await axios.get('/trader/ext/sumsub_token');
 			this.SumSubToken = token.data.token;
@@ -137,7 +62,7 @@ export default {
 								--success-color: #06d6a0;
 								--red-color: #ef476f;
 							}
-							
+
 							input, select, textarea {
 								background: none;
 								border-bottom: 1px solid #7a7a7aab !important;
@@ -145,9 +70,9 @@ export default {
 							input:focus, select:focus, textarea:focus {
 								border-bottom-color: var(--primary-color);
 							}
-							input::placeholder, textarea::placeholder {
-								color: (--primary-color) !important;
-								// opacity: 0.9 !important;
+							::placeholder {
+								color: var(--primary-color) !important;
+								opacity: 0.6 !important;
 							}
 							.input-field span, .radio-group span {
 								font-size: 11px;
@@ -161,19 +86,16 @@ export default {
 							.phone-input .phone {
 								box-shadow: none !important;
 							}
-							button:active:not(:disabled):not(.disabled), button:hover:not(:disabled):not(.disabled):not(:active) {
-								box-shadow: none;
-								transform: none;
-							}
-							radio:checked ~ .checkmark {
-								border: 2px solid var(--v-primary-color) !important;
+							input[type="radio"]:checked ~ .checkmark {
+								border: 2px solid var(--v-primary-base) !important;
 							}
 							.checkmark {
 								border: 2px solid var(--v-primary-base);
 							}
 							.radio-item .checkmark:after {
-								background-color: var(--accent-color);
+								background-color: var(--v-primary-base);
 							}
+
 							.show-hide {
 								top: 5px !important;
 								right: 3px !important;
@@ -205,6 +127,7 @@ export default {
 							}
 							button.submit {
 								border-radius: 5px;
+								border: none !important;
 								background: var(--v-primary-base) !important;
 							}
 							button.submit:hover {
@@ -216,36 +139,42 @@ export default {
 							button.submit .arrow {
 								margin-left: 4px;
 							}
+							button:active:not(:disabled):not(.disabled), button:hover:not(:disabled):not(.disabled):not(:active) {
+								box-shadow: none;
+								transform: none;
+							}
 
 							.content {
 								background: none;
+								margin: 0;
+  						  min-width: 100vw;
+							}
+							.transparent.desktop {
+								display: none;
+							}
+							.iframe2 {
+								margin: 0;
 							}
 							`,
-						// URL to css file in case you need change it dynamically from the code
-						// the similar setting at Applicant flow will rewrite customCss
-						// you may also use to pass string with plain styles `customCssStr:`
 					},
 					onError: error => {
 						console.error('WebSDK onError', error);
 					},
 				})
 				.build();
-			// you are ready to go:
-			// just launch the WebSDK by providing the container element for it
-			snsWebSdkInstance.launch('#sumsub-websdk-container');
+			snsWebSdkInstance.launch('#sumsub');
 		},
 	},
 
-	async mounted() {
-		let self = this;
+	async created() {
 		let token = await this.newSumSubToken();
 		this.launchWebSdk(
-			self.api,
-			self.flowName,
+			this.api,
+			this.flowName,
 			token,
-			self.email,
-			self.phone,
-			self.i18n
+			this.email,
+			this.phone,
+			this.i18n
 		);
 	},
 };
