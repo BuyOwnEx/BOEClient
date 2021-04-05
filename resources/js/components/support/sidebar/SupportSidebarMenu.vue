e23fdf5af22b5bc48ff3a01809ea2807bff9fb76
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-04-05 16:22:44 +0500

    support: remove useless wrapper

--- resources/js/components/support/sidebar/SupportSidebarMenu.vue
+++ resources/js/components/support/sidebar/SupportSidebarMenu.vue
@@ -1,65 +1,63 @@
 <template>
 	<div class="support-sidebar-menu pa-1 pt-0">
-		<div class="support-sidebar-menu__fixed-wrapper">
-			<div class="mt-2 mb-3">
-				<SupportDialogCreate />
-			</div>
-
-			<v-list class="mt-2 pa-0" dense nav>
-				<v-list-item
-					v-for="status in supportStatuses"
-					:key="status.id"
-					:input-value="selectedStatus === status.key"
-					:ripple="false"
-					active-class="primary--text"
-					@click="updateStatus(status.key)"
-				>
-					<v-list-item-icon>
-						<v-icon :color="status.color" small>{{ status.icon }}</v-icon>
-					</v-list-item-icon>
-
-					<v-list-item-content>
-						<v-list-item-title>{{ status.name }}</v-list-item-title>
-					</v-list-item-content>
-
-					<v-list-item-action v-if="showQuantity(status.key)">
-						<v-badge
-							color="primary"
-							class="font-weight-bold"
-							:content="getQuantity(status.key)"
-							inline
-						>
-						</v-badge>
-					</v-list-item-action>
-				</v-list-item>
-			</v-list>
-
-			<v-list dense nav class="mt-2 pa-0">
-				<div class="overline pa-1 mt-2">Приоритет</div>
-
-				<v-list-item
-					v-for="priority in priorityList"
-					:key="priority.id"
-					:input-value="selectedPriority === priority.key"
-					:ripple="false"
-					active-class="primary--text"
-					@click="updatePriority(priority.key)"
-				>
-					<v-list-item-icon>
-						<v-icon v-if="priority.key === 'all'" small :color="priority.color">
-							mdi-dots-horizontal
-						</v-icon>
-						<v-icon v-else small :color="priority.color">
-							mdi-label-outline
-						</v-icon>
-					</v-list-item-icon>
-
-					<v-list-item-content>
-						<v-list-item-title>{{ priority.name }}</v-list-item-title>
-					</v-list-item-content>
-				</v-list-item>
-			</v-list>
+		<div class="mt-2 mb-3">
+			<SupportDialogCreate />
 		</div>
+
+		<v-list class="mt-2 pa-0" dense nav>
+			<v-list-item
+				v-for="status in supportStatuses"
+				:key="status.id"
+				:input-value="selectedStatus === status.key"
+				:ripple="false"
+				active-class="primary--text"
+				@click="updateStatus(status.key)"
+			>
+				<v-list-item-icon>
+					<v-icon :color="status.color" small>{{ status.icon }}</v-icon>
+				</v-list-item-icon>
+
+				<v-list-item-content>
+					<v-list-item-title>{{ status.name }}</v-list-item-title>
+				</v-list-item-content>
+
+				<v-list-item-action v-if="showQuantity(status.key)">
+					<v-badge
+						color="primary"
+						class="font-weight-bold"
+						:content="getQuantity(status.key)"
+						inline
+					>
+					</v-badge>
+				</v-list-item-action>
+			</v-list-item>
+		</v-list>
+
+		<v-list dense nav class="mt-2 pa-0">
+			<div class="overline pa-1 mt-2">Приоритет</div>
+
+			<v-list-item
+				v-for="priority in priorityList"
+				:key="priority.id"
+				:input-value="selectedPriority === priority.key"
+				:ripple="false"
+				active-class="primary--text"
+				@click="updatePriority(priority.key)"
+			>
+				<v-list-item-icon>
+					<v-icon v-if="priority.key === 'all'" small :color="priority.color">
+						mdi-dots-horizontal
+					</v-icon>
+					<v-icon v-else small :color="priority.color">
+						mdi-label-outline
+					</v-icon>
+				</v-list-item-icon>
+
+				<v-list-item-content>
+					<v-list-item-title>{{ priority.name }}</v-list-item-title>
+				</v-list-item-content>
+			</v-list-item>
+		</v-list>
 	</div>
 </template>
 
