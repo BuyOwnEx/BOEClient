41a3188634ed7ea484c9680f06b6d64ab2a84b00
BuyOwnEx <have-dream@yandex.ru>
2021-04-08 19:14:34 +0300

    add editor in create dialog

--- resources/js/components/support/dialog/SupportDialogCreate.vue
+++ resources/js/components/support/dialog/SupportDialogCreate.vue
@@ -30,25 +30,131 @@
 
 					<v-divider />
 
-					<!--					<v-textarea-->
-					<!--						v-model="form.body"-->
-					<!--						:placeholder="$t('common.description')"-->
-					<!--						:rules="[rules.required]"-->
-					<!--						hide-details-->
-					<!--						auto-grow-->
-					<!--						solo-->
-					<!--						flat-->
-					<!--					/>-->
-					<div class="editor">
-						<input
-							type="text"
-							v-model="editor.extensions.options.placeholder.emptyNodeText"
-						/>
-						<editor-content
-							class="editor__content"
-							ref="editor"
-							:editor="editor"
-						/>
+					<div class="ticket-editor">
+						<editor-menu-bar v-slot="{ commands, isActive }" :editor="editor">
+							<div class="pa-1">
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.bold() }"
+										@click="commands.bold"
+								>
+									<v-icon>mdi-format-bold</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.italic() }"
+										@click="commands.italic"
+								>
+									<v-icon>mdi-format-italic</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.strike() }"
+										@click="commands.strike"
+								>
+									<v-icon>mdi-format-strikethrough</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.underline() }"
+										@click="commands.underline"
+								>
+									<v-icon>mdi-format-underline</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.paragraph() }"
+										@click="commands.paragraph"
+								>
+									<v-icon>mdi-format-paragraph</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.heading({ level: 1 }) }"
+										@click="commands.heading({ level: 1 })"
+								>
+									H1
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.heading({ level: 2 }) }"
+										@click="commands.heading({ level: 2 })"
+								>
+									H2
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.heading({ level: 3 }) }"
+										@click="commands.heading({ level: 3 })"
+								>
+									H3
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.bullet_list() }"
+										@click="commands.bullet_list"
+								>
+									<v-icon>mdi-format-list-bulleted</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.ordered_list() }"
+										@click="commands.ordered_list"
+								>
+									<v-icon>mdi-format-list-numbered</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.blockquote() }"
+										@click="commands.blockquote"
+								>
+									<v-icon>mdi-format-quote-close</v-icon>
+								</v-btn>
+
+								<v-btn
+										icon
+										tile
+										:class="{ 'is-active': isActive.code_block() }"
+										@click="commands.code_block"
+								>
+									<v-icon>mdi-code-tags</v-icon>
+								</v-btn>
+
+								<v-btn icon tile @click="commands.horizontal_rule">
+									<v-icon>mdi-minus</v-icon>
+								</v-btn>
+
+								<v-btn icon tile @click="commands.undo">
+									<v-icon>mdi-undo</v-icon>
+								</v-btn>
+
+								<v-btn icon tile @click="commands.redo">
+									<v-icon>mdi-redo</v-icon>
+								</v-btn>
+							</div>
+						</editor-menu-bar>
+						<editor-content class="editor__content pa-3 py-4" :editor="editor" />
 					</div>
 
 					<v-divider />
@@ -137,8 +243,26 @@
 <script>
 import { mapState, mapActions } from 'vuex';
 
-import { Editor, EditorContent } from 'tiptap';
-import { Placeholder } from 'tiptap-extensions';
+import { Editor, EditorContent, EditorMenuBar } from 'tiptap';
+import {
+	Blockquote,
+	CodeBlock,
+	HardBreak,
+	Heading,
+	HorizontalRule,
+	OrderedList,
+	BulletList,
+	ListItem,
+	TodoItem,
+	TodoList,
+	Bold,
+	Code,
+	Italic,
+	Link,
+	Strike,
+	Underline,
+	History,
+} from 'tiptap-extensions';
 
 import formValidationRules from '../../../mixins/common/formValidationRules';
 import loadingMixin from '../../../mixins/common/loadingMixin';
@@ -147,7 +271,10 @@ import errorNotificationMixin from '../../../mixins/common/errorNotificationMixi
 export default {
 	name: 'UserApiDialogCreate',
 
-	components: { EditorContent },
+	components: {
+		EditorContent,
+		EditorMenuBar,
+	},
 
 	mixins: [formValidationRules, loadingMixin, errorNotificationMixin],
 
@@ -165,13 +292,27 @@ export default {
 
 			editor: new Editor({
 				extensions: [
-					new Placeholder({
-						emptyEditorClass: 'is-editor-empty',
-						emptyNodeClass: 'is-empty',
-						emptyNodeText: 'Описание',
-					}),
+					new Blockquote(),
+					new BulletList(),
+					new CodeBlock(),
+					new HardBreak(),
+					new Heading({ levels: [1, 2, 3] }),
+					new HorizontalRule(),
+					new ListItem(),
+					new OrderedList(),
+					new TodoItem(),
+					new TodoList(),
+					new Link(),
+					new Bold(),
+					new Code(),
+					new Italic(),
+					new Strike(),
+					new Underline(),
+					new History(),
 				],
-				content: '',
+				content: `
+				  Default message
+				`,
 			}),
 		};
 	},
@@ -191,9 +332,16 @@ export default {
 					this.form.subject = '';
 					this.form.priority = null;
 					this.form.file = null;
-					this.editor.clearContent();
+					//this.editor.clearContent();
 					this.$refs.form.resetValidation();
 				});
+			}
+		},
+		clear(val) {
+			if (val) {
+				this.editor.clearContent();
+				this.file = null;
+				this.$emit('cleared');
 			}
 		},
 	},
@@ -235,7 +383,123 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss">
+.ticket-editor {
+		position: relative;
+
+		.v-btn {
+			&.is-active {
+				background-color: #f1f1f1;
+			}
+		}
+
+		.editor__content {
+			overflow-wrap: break-word;
+			word-wrap: break-word;
+			word-break: break-word;
+
+			* {
+				caret-color: currentColor;
+			}
+
+			.ProseMirror {
+				&:focus {
+					outline: none;
+				}
+			}
+
+			ul,
+			ol {
+				padding-left: 1rem;
+			}
+
+			li > p,
+			li > ol,
+			li > ul {
+				margin: 0;
+			}
+
+			a {
+				color: inherit;
+			}
+
+			blockquote {
+				border-left: 3px solid #b1b1b166;
+				color: rgba(0, 0, 0, 0.8);
+				padding-left: 0.8rem;
+				font-style: italic;
+
+				p {
+					margin: 0;
+				}
+			}
+
+			img {
+				max-width: 100%;
+				border-radius: 3px;
+			}
+
+			table {
+				border-collapse: collapse;
+				table-layout: fixed;
+				width: 100%;
+				margin: 0;
+				overflow: hidden;
+
+				td,
+				th {
+					min-width: 1em;
+					border: 2px solid #fafafa;
+					padding: 3px 5px;
+					vertical-align: top;
+					box-sizing: border-box;
+					position: relative;
+
+					> * {
+						margin-bottom: 0;
+					}
+				}
+
+				th {
+					font-weight: bold;
+					text-align: left;
+				}
+
+				.selectedCell:after {
+					z-index: 2;
+					position: absolute;
+					content: '';
+					left: 0;
+					right: 0;
+					top: 0;
+					bottom: 0;
+					background: rgba(200, 200, 255, 0.4);
+					pointer-events: none;
+				}
+
+				.column-resize-handle {
+					position: absolute;
+					right: -2px;
+					top: 0;
+					bottom: 0;
+					width: 4px;
+					z-index: 20;
+					background-color: #adf;
+					pointer-events: none;
+				}
+			}
+
+			.tableWrapper {
+				margin: 1em 0;
+				overflow-x: auto;
+			}
+
+			.resize-cursor {
+				cursor: ew-resize;
+				cursor: col-resize;
+			}
+		}
+	}
 .support-dialog-create {
 	&__file {
 		padding: 8px 12px;
@@ -243,25 +507,6 @@ export default {
 
 	::v-deep.v-input__slot {
 		padding: 0;
-	}
-
-	.editor {
-		position: relative;
-		padding: 12px;
-		&__content {
-			outline: none;
-			.ProseMirror:focus {
-				background: yellow !important;
-				outline: none !important;
-			}
-			p.is-editor-empty:first-child::before {
-				content: attr(data-empty-text);
-				float: left;
-				color: #aaa;
-				pointer-events: none;
-				height: 0;
-			}
-		}
 	}
 }
 </style>
