bcd4f8221c8d817bf685cad229d5890752088b17
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-04-09 17:35:08 +0500

    profile: add ref type select

--- resources/js/components/user/settings/UserSettingsTab.vue
+++ resources/js/components/user/settings/UserSettingsTab.vue
@@ -2,7 +2,9 @@
 	<v-card class="user-settings-tab tab-fill-height">
 		<v-card-title> {{ $t('user.title.settings') }} </v-card-title>
 
-		<v-card-text>
+		<CommonLoading v-if="!allRefTypes" />
+
+		<v-card-text v-else>
 			<section class="user-settings-tab__notifications">
 				<v-form @submit.prevent="saveNotificationsSetting">
 					<div class="user-settings-tab__header">
@@ -15,7 +17,50 @@
 						<v-radio :ripple="false" label="radio" />
 					</v-radio-group>
 
-					<v-btn type="submit" class="mt-3">{{ $t('common.save') }}</v-btn>
+					<v-btn type="submit" class="mt-3" :loading="saveNotificationsLoading">
+						{{ $t('common.save') }}
+					</v-btn>
+				</v-form>
+			</section>
+
+			<v-divider class="my-4" />
+
+			<section class="user-settings-tab__system">
+				<v-form>
+					<div class="user-settings-tab__header">
+						{{ $t('user.settings.headers.ref_settings') }}
+					</div>
+
+					<v-select
+						v-model="selectedRefTypeID"
+						:items="activeRefTypes"
+						item-value="id"
+						hide-details
+						hide-selected
+					>
+						<template #item="{item}">
+							{{ getRefText(item.id) }}
+						</template>
+						<template #selection="{item}">
+							{{ getRefText(item.id) }}
+						</template>
+					</v-select>
+					<div class="user-settings-tab__select-hint">
+						Получение <b>{{ selectedRefType.percent }}</b>
+						% от сделок рефералов на протяжении
+						<b>{{ selectedRefType.days }}</b> дней
+					</div>
+
+					<CommonDialog @confirm="saveRefSettings">
+						<template #default>
+							<v-btn class="mt-3" :loading="saveRefLoading">
+								{{ $t('common.save') }}
+							</v-btn>
+						</template>
+						<template #content>
+							{{ $t('user.settings.dialog.ref_description') }}
+						</template>
+					</CommonDialog>
 				</v-form>
 			</section>
 
@@ -33,7 +78,9 @@
 						<v-radio :ripple="false" label="radio" />
 					</v-radio-group>
 
-					<v-btn type="submit" class="mt-3">{{ $t('common.save') }}</v-btn>
+					<v-btn type="submit" class="mt-3" :loading="saveSystemLoading">
+						{{ $t('common.save') }}
+					</v-btn>
 				</v-form>
 			</section>
 
@@ -51,7 +98,9 @@
 						<v-radio :ripple="false" label="radio" />
 					</v-radio-group>
 
-					<v-btn type="submit" class="mt-3">{{ $t('common.save') }}</v-btn>
+					<v-btn type="submit" class="mt-3" :loading="saveSecurityLoading">
+						{{ $t('common.save') }}
+					</v-btn>
 				</v-form>
 			</section>
 		</v-card-text>
@@ -59,22 +108,108 @@
 </template>
 
 <script>
+import CommonDialog from '../../common/CommonDialog';
+import CommonLoading from '../../common/CommonLoading';
+
 export default {
 	name: 'UserSettingsTab',
 
+	components: { CommonDialog, CommonLoading },
+
 	data() {
-		return {};
+		return {
+			selectedRefTypeID: 1,
+			allRefTypes: null,
+
+			saveNotificationsLoading: false,
+			saveRefLoading: false,
+			saveSystemLoading: false,
+			saveSecurityLoading: false,
+		};
+	},
+
+	computed: {
+		activeRefTypes() {
+			if (!this.allRefTypes) return [];
+			return this.allRefTypes.filter(t => t.status === 'active');
+		},
+		selectedRefType() {
+			return this.activeRefTypes.find(t => t.id === this.selectedRefTypeID);
+		},
+	},
+
+	created() {
+		this.fetchRefTypes();
+		this.selectedRefType = this.$store.state.app.trader.refProgram;
 	},
 
 	methods: {
+		async fetchRefTypes() {
+			const { data } = await axios.get('/trader/ext/all_referral_types');
+			this.allRefTypes = data.data;
+		},
+
 		async saveNotificationsSetting() {
-			console.log('saveNotificationsSetting');
+			try {
+				this.saveNotificationsLoading = true;
+			} finally {
+				this.saveNotificationsLoading = false;
+			}
+		},
+		async saveRefSettings() {
+			try {
+				this.saveRefLoading = true;
+				await axios.post(
+					'/trader/ext/set_referral_type',
+					this.selectedRefTypeID
+				);
+			} finally {
+				this.saveRefLoading = false;
+			}
 		},
 		async saveSystemSettings() {
-			console.log('saveSystemSettings');
+			try {
+				this.saveSystemLoading = true;
+			} finally {
+				this.saveSystemLoading = false;
+			}
 		},
 		async saveSecuritySettings() {
-			console.log('saveSecuritySettings');
+			try {
+				this.saveSecurityLoading = true;
+			} finally {
+				this.saveSecurityLoading = false;
+			}
+		},
+
+		getRefText(id) {
+			const types = [
+				{
+					id: 1,
+					name: 'type1',
+				},
+				{
+					id: 2,
+					name: 'type2',
+				},
+				{
+					id: 3,
+					name: 'type3',
+				},
+				{
+					id: 4,
+					name: 'type4',
+				},
+				{
+					id: 5,
+					name: 'type5',
+				},
+				{
+					id: 6,
+					name: 'type6',
+				},
+			];
+			return types.find(t => t.id === id).name;
 		},
 	},
 };
@@ -85,6 +220,8 @@ export default {
 	&__header
 		font-size: 0.9rem
 		font-weight: 500
+	&__select-hint
+		padding-top: 4px
 
 	::v-deep.v-input--selection-controls
 		margin-top: 4px
