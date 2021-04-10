7e4bfffa9a8c0eb9e6db76986c47964021bf98c7
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-04-10 07:37:35 +0500

    common: remove comments from copyLabel component, add empty lines

--- resources/js/components/common/CopyLabel.vue
+++ resources/js/components/common/CopyLabel.vue
@@ -15,46 +15,31 @@
 </template>
 
 <script>
-/*
-|---------------------------------------------------------------------
-| Copy Label Component
-|---------------------------------------------------------------------
-|
-| Copy to clipboard with the plugin clipboard `this.$clipboard`
-|
-*/
 export default {
 	name: 'CopyLabel',
 
 	props: {
-		// Text to copy to clipboard
 		text: {
 			type: String,
 			default: '',
 		},
-		// Text to show on toast
 		toastText: {
 			type: String,
 			default: 'Copied to clipboard!',
 		},
-		/**
-		 * CSS animation with animate.css
-		 * https://animate.style/
-		 */
-		animation: {
-			type: String,
-			default: 'heartBeat',
-		},
 	},
+
 	data() {
 		return {
 			tooltip: 'Copy',
 			timeout: null,
 		};
 	},
+
 	beforeDestroy() {
 		if (this.timeout) clearTimeout(this.timeout);
 	},
+
 	methods: {
 		copy() {
 			this.$clipboard(this.text, this.toastText);
