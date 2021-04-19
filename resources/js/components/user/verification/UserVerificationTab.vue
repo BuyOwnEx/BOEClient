8c683a5c889cf0d63eecfde57f17cccc396ed776
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-04-19 19:21:07 +0500

    profile-verification: fix links path

--- resources/js/components/user/verification/UserVerificationTab.vue
+++ resources/js/components/user/verification/UserVerificationTab.vue
@@ -21,8 +21,8 @@ export default {
 		phone: null,
 		i18n: null,
 
-		customCssLink: `${window.location.protocol}//${window.location.host}/css/sumsub.css`,
-		customCssDarkLink: `${window.location.protocol}//${window.location.host}/css/sumsubDark.css`,
+		customCssLink: `${window.location.protocol}//${window.location.host}/dist/css/sumsub.css`,
+		customCssDarkLink: `${window.location.protocol}//${window.location.host}/dist/css/sumsubDark.css`,
 	}),
 
 	computed: {
