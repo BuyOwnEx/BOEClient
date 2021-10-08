f2b77ff40b1598998b7026988bc514c333cc915a
Selithrarion <sergey.maltsev2004@yandex.ru>
2021-10-08 06:56:52 +0500

    trading: fix mobile forms rtl

--- resources/js/components/trading/forms/TradingFormsWrapper.vue
+++ resources/js/components/trading/forms/TradingFormsWrapper.vue
@@ -32,7 +32,7 @@
 
 		<!--	mobile version	-->
 		<div v-else>
-			<v-tabs v-model="selectedTab" class='small-tabs' :key="$i18n.locale" grow>
+			<v-tabs v-model="selectedTab" class="small-tabs" :key="$i18n.locale" grow>
 				<v-tab :key="1">
 					{{ $t('trading.order.direction.buy') }}
 				</v-tab>
@@ -139,6 +139,8 @@ export default {
 			&--sell
 				padding-left: 0
 				padding-right: 8px
+		::v-deep &__best-wrapper
+			float: left
 
 @media screen and (min-width: 960px)
 	.trading-forms
